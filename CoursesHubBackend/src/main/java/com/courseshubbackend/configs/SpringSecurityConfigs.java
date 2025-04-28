fdec3eb0d2d5c3c2a84b9306f13a9a07c5d777cb
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2025-04-28 09:44:36 +0700

    feat:add Spring Security

---
+++ CoursesHubBackend/src/main/java/com/courseshubbackend/configs/SpringSecurityConfigs.java
@@ -0,0 +1,54 @@
+package com.courseshubbackend.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+        "com.courseshubbackend.controllers",
+        "com.courseshubbackend.repositories",
+        "com.courseshubbackend.services"
+})
+public class SpringSecurityConfigs {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector(){
+        return new HandlerMappingIntrospector();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/").authenticated()
+                        .requestMatchers("/css/**", "/js/**", "/images/**",  "/configs/**").permitAll()
+                        .requestMatchers("/api/**").permitAll())
+                .formLogin(form -> form.loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/login?error=true").permitAll())
+                .logout(logout
+                        -> logout.logoutSuccessUrl("/login").permitAll());
+        return http.build();
+    }
+}
