0ee4bf04d83b724b8c9094223e44b0112a7f5e79
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2025-05-17 23:22:00 +0700

    feat: add Bean for Cloudinary and config authorize for admin panel

--- CoursesHubBackend/src/main/java/com/courseshubbackend/configs/SpringSecurityConfigs.java
+++ CoursesHubBackend/src/main/java/com/courseshubbackend/configs/SpringSecurityConfigs.java
@@ -1,9 +1,14 @@
 package com.courseshubbackend.configs;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,7 +27,10 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
         "com.courseshubbackend.repositories",
         "com.courseshubbackend.services"
 })
+@PropertySource("classpath:application.properties")
 public class SpringSecurityConfigs {
+    @Autowired
+    private Environment env;
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -38,14 +46,26 @@ public class SpringSecurityConfigs {
     }
 
     @Bean
+    public Cloudinary cloudinary() {
+        Cloudinary cloudinary
+                = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", env.getProperty("cloudinary.cloud.name"),
+                "api_key", env.getProperty("cloudinary.api.key"),
+                "api_secret", env.getProperty("cloudinary.api.secret"),
+                "secure", true));
+        return cloudinary;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/").authenticated()
-                        .requestMatchers("/courses").authenticated()
-                        .requestMatchers("/teachers").authenticated()
-                        .requestMatchers("/students").authenticated()
-                        .requestMatchers("/categories").authenticated()
-                        .requestMatchers("/css/**", "/js/**", "/images/**",  "/configs/**").permitAll()
+                        .requestMatchers("/").hasRole("ADMIN")
+                        .requestMatchers("/courses/**").hasRole("ADMIN")
+                        .requestMatchers("/teachers/**").hasRole("ADMIN")
+                        .requestMatchers("/students/**").hasRole("ADMIN")
+                        .requestMatchers("/categories/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/courses/**").hasRole("ADMIN")
+                        .requestMatchers("/css/**", "/js/**", "/images/**",  "/configs/**", "/templates/**").permitAll()
                         .requestMatchers("/api/**").permitAll())
                 .formLogin(form -> form.loginPage("/login")
                         .loginProcessingUrl("/login")
